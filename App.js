a8f4bbce682b3073a38ec5f47928f23bb8ac57ea
shameer <shameer.myd@gmail.com>
2025-10-09 12:44:32 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,11 @@
+/**
+ * <div id = "pa">
+ * 
+ */
+
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div",{id:"child"},"child DIV")
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
