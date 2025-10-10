ee18be747e48256fd1ed359c93c2adf68700dee7
shameer <shameer.myd@gmail.com>
2025-10-10 12:05:31 +0530

    ep 2

--- App.js
+++ App.js
@@ -2,6 +2,8 @@
  * 
  * 
  */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("div",{id:"child"},"child DIV")
